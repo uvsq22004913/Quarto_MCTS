8f131d0424c407b906e59223570be1f4f7db2ec0
Billal Medour <bilalmedour60@gmail.com>
2024-04-07 01:27:53 +0200

    implémentation de la classe Noeud

--- mcts.py
+++ mcts.py
@@ -7,26 +7,6 @@ piece = {"grand" : 1,
          "clair" : 1,
          "carré" : 1}
 
-plateau = [
-            [],
-            [],
-            [],
-            []
-          ]
-
-def placer_une_piece (plateau, piece , position):
-    """
-        @paramètres:
-            
-piece est la piece a placer sur le plateau est de la forme [b0, b1, b2, b3] avec bi des 0 ou des 1
-position est la position ou il faut placer la pièce elle de la forme (ligne, colonne)
-le plateau sur lequel on place la pièce
-
-        @retour:
-            
-Le plateau avec la pièce placée"""
-  pass
-
 
 # entrée : racine (1)
 # sortie : noeud sélectionné (2)
@@ -59,5 +39,28 @@ def quarto():
 
 #Structure de l'arbre
 class Noeud:
-   def __init__(self, plateau, enfants, parent, p_reste, UCT, nbr_éssaies, victoire):
+    def __init__(self, plateau, enfants, parent, p_reste):
+        self.plateau    = plateau
+        self.enfants    = enfants
+        self.parent     = parent
+        self.p_reste    = p_reste
+        self.uct        = -1      # uct = -1 représente uct --> infinie
+        self.simulation = 0
+        self.victoire   = 0
 
+    def get_parent(self):
+        return self.parent
+    
+    def get_enfants(self):
+        return self.enfants
+    
+    def get_plataeu(self):
+        return self.plateau
+
+# initialisation d'un plateau vide
+plateau = [
+            [],
+            [],
+            [],
+            []
+          ]
